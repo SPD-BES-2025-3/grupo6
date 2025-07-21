33458bb403f960084a3439ecc54a465de9ef1a82
arthur <arthurfpeixoto@gmail.com>
2025-07-21 14:06:20 -0300

    feat: configuração inicial electron para rodar em desktop

---
+++ front/main.js
@@ -0,0 +1,18 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+    width: 800,
+    height: 600, 
+    });
+    win.maximize();
+    win.show();
+    win.loadURL('http://localhost:3000'); 
+    win.removeMenu(); 
+  
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
