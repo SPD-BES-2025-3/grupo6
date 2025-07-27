0cb42bb1d7170c2c12bd997737b717ef736be004
Arthur Faria <arthurfpeixoto@gmail.com>
2025-07-27 12:50:54 -0300

    reajuste de colunas listagem exemlares (backend foi corrigido e volta os dados do livro)

--- front/src/components/datatable/TabelaListaExemplares.jsx
+++ front/src/components/datatable/TabelaListaExemplares.jsx
@@ -57,21 +57,21 @@ const TabelaListaExemplares = ({ permissao = false, size = 12 }) => {
             headerName: "Código de identificação",
             flex: 1,
         },
-        // {
-        //     field: "livroId",
-        //     headerName: "Código do livro",
-        //     flex: 1,
-        // },
-        // {
-        //     field: "livroNome",
-        //     headerName: "Titulo",
-        //     flex: 1,
-        // },
-        // {
-        //     field: "livroAutor",
-        //     headerName: "Autor",
-        //     flex: 1.2,
-        // },
+        {
+            field: "livroId",
+            headerName: "Código do livro",
+            flex: 1,
+        },
+        {
+            field: "livroNome",
+            headerName: "Titulo",
+            flex: 1,
+        },
+        {
+            field: "livroAutor",
+            headerName: "Autor",
+            flex: 1.2,
+        },
         {
             field: "conservacao",
             headerName: "Estado de conservação",
